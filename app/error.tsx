68c0b7b76be96e6d85324e067d88a715e92270eb
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-04-25 15:28:06 +0600

    imported heroUI template and modified

---
+++ app/error.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { useEffect } from 'react';
+
+interface ErrorProps {
+	error: Error;
+	reset: () => void;
+}
+
+export default function Error({ error, reset }: ErrorProps) {
+	useEffect(() => console.error(error), [error]);
+
+	return (
+		<div>
+			<h2>Something went wrong!</h2>
+			<button
+				onClick={
+					// Attempt to recover by trying to re-render the segment
+					() => reset()
+				}
+			>
+				Try again
+			</button>
+		</div>
+	);
+}
---
+++ config/site.ts
@@ -0,0 +1,66 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+	name: 'Nazmul Hassan Portfolio',
+	description: 'Portfolio Website of Nazmul Hassan',
+	navItems: [
+		{
+			label: 'Home',
+			href: '/',
+		},
+		{
+			label: 'Docs',
+			href: '/docs',
+		},
+		{
+			label: 'Pricing',
+			href: '/pricing',
+		},
+		{
+			label: 'Blog',
+			href: '/blog',
+		},
+		{
+			label: 'About',
+			href: '/about',
+		},
+	],
+	navMenuItems: [
+		{
+			label: 'Profile',
+			href: '/profile',
+		},
+		{
+			label: 'Dashboard',
+			href: '/dashboard',
+		},
+		{
+			label: 'Projects',
+			href: '/projects',
+		},
+		{
+			label: 'Team',
+			href: '/team',
+		},
+		{
+			label: 'Calendar',
+			href: '/calendar',
+		},
+		{
+			label: 'Settings',
+			href: '/settings',
+		},
+		{
+			label: 'Help & Feedback',
+			href: '/help-feedback',
+		},
+		{
+			label: 'Logout',
+			href: '/logout',
+		},
+	],
+	links: {
+		github: 'https://github.com/nazmul-nhb',
+		discord: 'https://discord.com/users/831030314528538664',
+	},
+};
