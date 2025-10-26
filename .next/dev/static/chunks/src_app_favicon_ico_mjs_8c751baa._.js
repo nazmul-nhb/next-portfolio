185dfd553ff8d37937bdd38e0548ffd9981e8eb1
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-10-26 20:22:26 +0600

    update(gitignore): updated gitignore file

--- .next/dev/static/chunks/src_app_favicon_ico_mjs_8c751baa._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/a045d_next_dist_6c400e37._.js"
-],
-    source: "dynamic"
-});
