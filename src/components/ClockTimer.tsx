cd1005ca2c58e3c9c923f317424e329fc7ba2d2a
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-11-04 12:25:56 +0600

    🧼 refactor(hijibiji): tested nhb-hooks and removed unnecessary code

--- src/components/ClockTimer.tsx
+++ src/components/ClockTimer.tsx
@@ -1,11 +1,9 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import { formatTimerUnit } from '@/lib/utils';
-import { Clock } from 'lucide-react';
-import { useClock, useTimer } from 'nhb-hooks';
+import { Clock, Copy, CopyCheck } from 'lucide-react';
+import { formatTimer, useClock, useCopyText, useTimer } from 'nhb-hooks';
 import { Chronos } from 'nhb-toolbox';
-import { pluralizer } from 'nhb-toolbox/pluralizer';
 import { useEffect, useState } from 'react';
 
 export default function ClockTimer() {
@@ -15,12 +13,17 @@ export default function ClockTimer() {
 		format: 'dd, mmm DD, yyyy - hh:mm:ss a',
 	});
 
+	const { copiedText, copyToClipboard } = useCopyText({
+		onSuccess(msg) {
+			console.log(msg);
+		},
+	});
+
 	const duration = useTimer(new Chronos('2025-11-11').endOf('day'));
 
 	useEffect(() => setMounted(true), []);
 
 	if (!mounted) return null;
-
 	return (
 		<span className="w-full flex flex-col gap-4 font-semibold items-start">
 			<span>
@@ -28,9 +31,7 @@ export default function ClockTimer() {
 			</span>
 			<span>
 				Deadline Ends in{' '}
-				<span className="text-red-600 animate-pulse">
-					{formatTimerUnit(duration, { separator: ' · ', maxUnits: 6 })}
-				</span>
+				<span className="text-red-600 animate-pulse">{formatTimer(duration)}</span>
 			</span>
 			<Button
 				variant={isPaused ? 'default' : 'destructive'}
@@ -38,6 +39,21 @@ export default function ClockTimer() {
 				onClick={isPaused ? resume : pause}
 			>
 				{isPaused ? 'Resume ' : 'Pause '} Clock <Clock fontWeight={900} />
+			</Button>
+
+			<Button
+				// size={copiedText ? 'default' : 'icon'}
+				onClick={() => copyToClipboard(formatted)}
+			>
+				{copiedText ? (
+					<>
+						{copiedText} <CopyCheck />
+					</>
+				) : (
+					<>
+						Copy Current Date <Copy />
+					</>
+				)}
 			</Button>
 		</span>
 	);
