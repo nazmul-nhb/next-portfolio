f46da8c3f603e154325fbfcfd742398cb96e878e
Nazmul Hassan <nazmulnhb007@yahoo.com>
2026-04-03 23:25:38 +0600

    🔧 update(robots): modified src/app/robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,17 +1,13 @@
 import type { MetadataRoute, Route } from 'next';
-import { siteConfig } from '@/configs/site';
 import { buildCanonicalUrl } from '@/lib/utils';
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: [
-            {
-                userAgent: '*',
-                allow: '/',
-                disallow: ['/admin', '/api', '/auth', '/messages'],
-            },
-        ],
+        rules: {
+            userAgent: '*',
+            allow: '/',
+            disallow: ['/admin', '/api', '/auth', '/messages'],
+        },
         sitemap: buildCanonicalUrl('/sitemap.xml' as Route),
-        host: siteConfig.baseUrl,
     };
 }
